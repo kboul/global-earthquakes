f7aa72ad707191c523f00b3afa2f97023b31dfec
kboul <kos.voulgaridis@gmail.com>
2019-09-14 19:44:08 +0300

    Change SERVER_URL to API_URL on httpService file

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // define app API url
-axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 // configure headers
 axios.interceptors.request.use(
     config => {
