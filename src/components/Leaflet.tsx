ffa8ebe8c4c5c720dd17e4374f557f3b60bd167e
kboul <kos.voulgaridis@gmail.com>
2019-09-14 21:33:16 +0300

    Create Leaflet component

--- src/components/Leaflet.tsx
+++ src/components/Leaflet.tsx
@@ -3,11 +3,11 @@ import { Map, TileLayer } from 'react-leaflet';
 
 export interface LeafletProps {}
 
-const position: [number, number] = [51.505, -0.09];
+const position: [number, number] = [0, 0];
 
 const Leaflet: React.SFC<LeafletProps> = () => {
     return (
-        <Map center={position} zoom={13} style={{ height: '100vh' }}>
+        <Map center={position} zoom={3} style={{ height: '100vh' }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
