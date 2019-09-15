f29924d4f12921560f0fc9804df80a2a38a41e83
kboul <kos.voulgaridis@gmail.com>
2019-09-15 15:55:32 +0300

    Create a Legend component to explain the magnitude colors

---
+++ src/components/Legend.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import L from 'leaflet';
+import { withLeaflet } from 'react-leaflet';
+import { circleMarkerColor } from '../utils/circleMarkerColor';
+import '../styles/Legend.css';
+
+export interface LegendProps {
+    leaflet: {
+        map: any;
+    };
+}
+
+const Legend: React.SFC<LegendProps> = ({ leaflet }) => {
+    const legend = new L.Control({ position: 'bottomright' });
+
+    legend.onAdd = (map: any) => {
+        console.log(map);
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 1, 3, 4, 7],
+            labels = [],
+            from,
+            to;
+
+        labels.push('<h4>Magnitude</h4>');
+
+        for (let i = 0; i < grades.length; i++) {
+            from = grades[i];
+            to = grades[i + 1];
+
+            labels.push(
+                '<i style="background:' +
+                    circleMarkerColor(from + 1) +
+                    '"></i> ' +
+                    from +
+                    (to ? '&ndash;' + to : '+')
+            );
+        }
+
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+
+    legend.addTo(leaflet.map);
+
+    return null;
+};
+
+export default withLeaflet(Legend);
