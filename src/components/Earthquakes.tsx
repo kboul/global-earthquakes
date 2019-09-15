836990650ec3528263a421aeb44826c7588a32de
kboul <kos.voulgaridis@gmail.com>
2019-09-15 21:13:50 +0300

    Display earthquake time in GMT+3 greek time on circle marker popup

--- src/components/Earthquakes.tsx
+++ src/components/Earthquakes.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { withLeaflet } from 'react-leaflet';
 import L, { LatLng, Layer } from 'leaflet';
-import { getEarthquakes } from '../services/getEarthquakes';
+import { withLeaflet } from 'react-leaflet';
 import { IFeature } from '../models/IFEature';
+import { getEarthquakes } from '../services/getEarthquakes';
+import { timeConverter } from '../utils/timeConverter';
 import { geojsonMarkerOptions } from '../utils/geojsonMarkerOptions';
 
 export interface EarthquakesProps {
@@ -19,12 +20,17 @@ const Earthquakes: React.SFC<EarthquakesProps> = ({ leaflet }) => {
         let popupContent = `
             <h3>${feature.properties.title}</h3>
             <b>Place</b>: ${feature.properties.place} <br>
-            <b>Time</b>: <br>
+            <b>Time (GMC+3)</b>: ${timeConverter(
+                feature.properties.time,
+                3
+            )} <br>
             <b>Lat</b>: ${feature.geometry.coordinates[1]}
             <b>Lon</b>: ${feature.geometry.coordinates[0]} <br>
             <b>Depth</b>: ${feature.geometry.coordinates[2]} km <br>
             <b>Magnitude</b>: ${feature.properties.mag} Richter <br>
-            <b>Details</b>: <a href=${feature.properties.url}>click here to see more details</a>
+            <b>Details</b>: <a href=${
+                feature.properties.url
+            }>click here to see more details</a>
         `;
 
         if (feature.properties) layer.bindPopup(popupContent);
