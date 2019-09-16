30029ad855895cf680db1359943825d5e97a74bd
kboul <kos.voulgaridis@gmail.com>
2019-09-16 15:52:32 +0300

    Install bootstrap & import bootstrap.css on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
 import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
