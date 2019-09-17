7226fd29857cd3665ff58890fe0edb87a0d759fe
kboul <kos.voulgaridis@gmail.com>
2019-09-17 20:43:11 +0300

    Create changeEndtime action, action type, implement logic on earthquakes reducer for this action

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,6 +1,11 @@
-import { CHANGE_STARTTIME } from './types';
+import { CHANGE_STARTTIME, CHANGE_ENDTIME } from './types';
 
 export const changeStarttime = (starttime: string) => ({
     type: CHANGE_STARTTIME,
     starttime
+});
+
+export const changeEndtime = (endtime: string) => ({
+    type: CHANGE_ENDTIME,
+    endtime
 });
--- src/store/reducers/earthquakesReducer.ts
+++ src/store/reducers/earthquakesReducer.ts
@@ -1,7 +1,8 @@
-import { CHANGE_STARTTIME } from '../actions/types';
+import { CHANGE_STARTTIME, CHANGE_ENDTIME } from '../actions/types';
 
 const initialState = {
-    starttime: 'NOW - 3days'
+    starttime: 'NOW - 3days',
+    endtime: ''
 };
 
 export default (state = initialState, action: any) => {
@@ -10,6 +11,11 @@ export default (state = initialState, action: any) => {
             return {
                 ...state,
                 starttime: action.starttime
+            };
+        case CHANGE_ENDTIME:
+            return {
+                ...state,
+                endtime: action.endtime
             };
         default:
             return state;
