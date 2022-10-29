d200535e68e8a3261009f94b7e12907a6188d1ca
kboul <kos.voulgaridis@gmail.com>
2022-10-29 23:09:53 +0300

    Upgrade all packages to their altest version apart from bootstrap

--- src/Leaflet/utils.ts
+++ src/Leaflet/utils.ts
@@ -1,27 +1,19 @@
-/* eslint-disable no-nested-ternary */
 const circleMarkerColor = (magnitude: number): string => {
-  return magnitude <= 1
-    ? '#00b800'
-    : magnitude > 1 && magnitude <= 2
-    ? '#b6fe00'
-    : magnitude > 2 && magnitude <= 3
-    ? '#f6ff00'
-    : magnitude > 3 && magnitude <= 5
-    ? '#ffcf00'
-    : magnitude > 5 && magnitude <= 7
-    ? '#ff9000'
-    : '#ff0000';
+  if (magnitude <= 1) return '#00b800';
+  if (magnitude > 1 && magnitude <= 2) return '#b6fe00';
+  if (magnitude > 2 && magnitude <= 3) return '#f6ff00';
+  if (magnitude > 3 && magnitude <= 5) return '#ffcf00';
+  if (magnitude > 5 && magnitude <= 7) return '#ff9000';
+  return '#ff0000';
 };
 
-const geojsonMarkerOptions = (magnitude: number): Object => {
-  return {
-    radius: 2.5 * magnitude,
-    fillColor: circleMarkerColor(magnitude),
-    color: 'grey',
-    weight: 0.5,
-    opacity: 1,
-    fillOpacity: 0.8
-  };
-};
+const geojsonMarkerOptions = (magnitude: number): Object => ({
+  radius: 2.5 * magnitude,
+  fillColor: circleMarkerColor(magnitude),
+  color: 'grey',
+  weight: 0.5,
+  opacity: 1,
+  fillOpacity: 0.8
+});
 
 export { circleMarkerColor, geojsonMarkerOptions };
