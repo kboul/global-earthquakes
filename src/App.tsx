fe384f65fe40fff16d716a97b891b9c6a0294e85
kboul <kos@voulgaridis@gmail.com>
2024-02-09 15:06:40 +0200

    Install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { createGlobalStyle } from 'styled-components';
 
 import { Map, Navbar } from './components';
 import queryClient from './queryClient';
+import './index.css';
 
 export default function App() {
   return (
-    <>
-      <GlobalStyle />
-      <QueryClientProvider client={queryClient}>
-        <Navbar />
-        <Map />
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Navbar />
+      <Map />
+    </QueryClientProvider>
   );
 }
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-  }
-`;
