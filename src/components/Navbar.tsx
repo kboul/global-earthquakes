a78e9fd46d4163e49918c5858733230d08969b59
kboul <kos.voulgaridis@gmail.com>
2025-01-04 23:28:22 +0200

    Add map icon and make it toggled next to settings icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useShallow } from "zustand/react/shallow";
 import { Cog6ToothIcon } from "@heroicons/react/24/solid";
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
 
 import { ResponsiveDialog } from "./ui/ResponsiveDialog";
 import { Select } from "./ui/Select";
@@ -9,7 +10,6 @@ import { RadioGroup } from "./ui/RadioGroup";
 import { useStore } from "../hooks";
 import { cn, convertDropdownValue } from "../utils";
 import { days, initialNumOfDays, initialStartTime } from "../constants";
-import { Tooltip } from "./ui/Tooltip";
 
 export default function Navbar() {
   const {
@@ -61,64 +61,69 @@ export default function Navbar() {
           Global Earthquakes
         </span>
 
-        <ResponsiveDialog
-          open={settingsOpen}
-          onOpenChange={(open) => setStore({ settingsOpen: open })}
-          title="Search by date"
-          Trigger={
-            <Cog6ToothIcon
-              className={cn(
-                "h-6 w-6 text-white cursor-pointer",
-                settingsOpen && "fill-[#0ff]"
-              )}
-              title="Settings"
-            />
-          }>
-          <div className="flex flex-col gap-y-4">
-            <RadioGroup
-              className="flex flex-row space-x-4"
-              value={selectedRadio}
-              onValueChange={handleDateSelectionsChange}
-              options={[
-                { value: "days", label: "Days" },
-                { value: "timePeriod", label: "Time period" }
-              ]}
-            />
-
-            <label
-              className="block text-sm font-medium text-gray-900 dark:text-white"
-              htmlFor="numberOfDays">
-              Number of days
-            </label>
-            <Select
-              disabled={selectedRadio === "timePeriod"}
-              onValueChange={handleChange}
-              options={days}
-              placeholder="Days"
-              value={numOfDays}
-            />
-
-            <label
-              className="block text-sm font-medium text-gray-900 dark:text-white"
-              htmlFor="timePeriod">
-              Time period
-            </label>
-            <div className="flex gap-x-4">
-              <AppInput
-                disabled={selectedRadio === "days"}
-                type="date"
-                onChange={(e) => setStartTime(e.target.value)}
-                value={startTime}
+        <div className="flex gap-2 cursor-pointer">
+          <GlobeAltIcon className="w-7 h-7 text-[#0ff]" title="Map" />
+          <ResponsiveDialog
+            open={settingsOpen}
+            onOpenChange={(open) => setStore({ settingsOpen: open })}
+            title="Search by date"
+            Trigger={
+              <div title="Settings">
+                <Cog6ToothIcon
+                  className={cn(
+                    "w-7 h-7 text-white",
+                    settingsOpen && "text-[#0ff]"
+                  )}
+                  title="Settings"
+                />
+              </div>
+            }>
+            <div className="flex flex-col gap-y-4">
+              <RadioGroup
+                className="flex flex-row space-x-4"
+                value={selectedRadio}
+                onValueChange={handleDateSelectionsChange}
+                options={[
+                  { value: "days", label: "Days" },
+                  { value: "timePeriod", label: "Time period" }
+                ]}
               />
-              <AppInput
-                disabled={selectedRadio === "days"}
-                type="date"
-                onChange={(e) => setEndTime(e.target.value)}
-                value={endTime}
+
+              <label
+                className="block text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="numberOfDays">
+                Number of days
+              </label>
+              <Select
+                disabled={selectedRadio === "timePeriod"}
+                onValueChange={handleChange}
+                options={days}
+                placeholder="Days"
+                value={numOfDays}
               />
+
+              <label
+                className="block text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="timePeriod">
+                Time period
+              </label>
+              <div className="flex gap-x-4">
+                <AppInput
+                  disabled={selectedRadio === "days"}
+                  type="date"
+                  onChange={(e) => setStartTime(e.target.value)}
+                  value={startTime}
+                />
+                <AppInput
+                  disabled={selectedRadio === "days"}
+                  type="date"
+                  onChange={(e) => setEndTime(e.target.value)}
+                  value={endTime}
+                />
+              </div>
             </div>
-          </div>
-        </ResponsiveDialog>
+          </ResponsiveDialog>
+        </div>
       </div>
     </nav>
   );
