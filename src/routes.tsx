2b9e98e57a58222888cb92ca4b31bd85b8d75cf3
kboul <kos.voulgaridis@gmail.com>
2025-01-05 17:32:38 +0200

    Fix react router v7 console errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,16 @@ import { createHashRouter } from "react-router-dom";
 
 import { HomePage, ErrorPage, Layout } from "./pages";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [{ index: true, element: <HomePage /> }]
-  }
-]);
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [{ index: true, element: <HomePage /> }]
+    }
+  ],
+  { future: { v7_relativeSplatPath: true } }
+);
 
 export default router;
