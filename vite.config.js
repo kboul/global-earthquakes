9cb0b82a42cec019991e230f0387fb2462135711
kboul <kos.voulgaridis@gmail.com>
2025-02-28 17:30:09 +0200

    Change docker configuration to listen to port 5173

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    // opens the broswer when the server starts
-    server: {
-      open: true
-    },
-    plugins: [react()]
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true, // opens the broswer when the server starts
+    host: "0.0.0.0", // Allows access from outside the container
+    port: 5173 // Explicitly set the port
+  }
 });
