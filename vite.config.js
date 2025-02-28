41652b82d01eac2ca272cc5519a3e796f579e5ba
kboul <kos.voulgaridis@gmail.com>
2025-02-28 12:36:28 +0200

    Remove create react app and introduce vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  return {
+    // opens the broswer when the server starts
+    server: {
+      open: true
+    },
+    build: {
+      outDir: "build"
+    },
+    plugins: [react()]
+  };
+});
